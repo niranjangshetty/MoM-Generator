fa54b4ef745864b8e88374c87fb930b3a5900d92
niranjangshetty <niranjangshetty@gmail.com>
2025-09-18 16:22:30 +0530

    Update imageextractor.py

--- imageextractor.py
+++ imageextractor.py
@@ -1,32 +1,80 @@
 import google.generativeai as genai
-import cv2
-from PIL import Image
 import os
-import numpy as np
+from pdfExtractor import text_extractor_pdf
+import streamlit as st
+from docxExtractor import text_extractor_docx
+from imageExtractor import extract_text_image
 
-def extract_text_image(image_path):
-    file_bytes = np.asarray(bytearray(image_path.read()), dtype=np.uint8)
-    image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-    # Lets load and process the image
-    #image = cv2.imread('notes1.jpg')
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # convert BGR to RGB
-    image_grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # convert BGR to greyscale
-    _, image_bw = cv2.threshold(image_grey, 140,255, cv2.THRESH_BINARY) # to convert to black and white
 
-    # The Image that cv2 gives is in numpy array format, we need to convert it to image object
-    final_image = Image.fromarray(image_bw)
+#configure the model
+key = os.getenv('GOOGE_API_KEY')
+genai.configure(api_key=key)
+model = genai.GenerativeModel('gemini-2.5-flash-lite')
 
-    # Configure Genai Model
-    key = os.getenv('GOOGLE_API_KEY')
-    genai.configure(api_key = key)
-    model = genai.GenerativeModel('gemini-2.5-flash-lite')
 
-    # Let's write prompt for OCR
-    prompt = '''You act as an OCR application on the given image and extract the text from it.
-            Give only the text as output, do not give any other explain or description. '''
-    
-    # Let's extract and return the text
-    response = model.generate_content([prompt, final_image])
-    output_text = response.text
-    return output_text
+# Upload file in sidebar
 
+user_text = None
+st.sidebar.title(':orange[Upload your MOM notes here : ]')
+st.sidebar.subheader('Only Image, PDFs and docs file are allowed to upload')
+user_file = st.sidebar.file_uploader("Upload your file", type =['pdf', 'jpg', 'docx', 'png', 'jpeg'])
+if user_file:
+    if user_file.type == 'application/pdf':
+
+        user_text = text_extractor_pdf(user_file)
+    elif user_file.type  == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+        user_text = text_extractor_docx(user_file) 
+        
+    elif user_file.type in ['image/jpg', 'image/jpeg', 'image/png']:
+        user_text = extract_text_image(user_file)
+    else:
+        st.sidebar.error('Upload correct file format')
+
+# MAIN PAGE :
+
+st.title(':blue["___MINUTES OF MEETING___"]\n :orange[*AI ASSISTED MOM GENERATOR IN A STANDARDIZED FORM FOR MEETING NOTES*]')
+tips = '''TIPS TO USE THIS APP:
+1. Upload your meeting in side bar (Image , PDF or DOCX)
+2. Click on generate MOM and get the standardized MOM's'''
+st.write(tips)
+
+
+if st.button('Generate MOM'):
+     if user_text is None:
+          st.error('Some error Occured - Text is not generated')
+
+     else:
+          with st.spinner('Processing your Data, it might take few Seconds..........'):
+               prompt = f'''Assume you are expert in Creating MINUTES OF MEETING. User has provided notes of meeting in text format .
+               Using this data , you need to create a standardized minutes of Meetings for the user
+            
+               It should be so specific and consice so anybody can easily understand without any confusions
+               Highlight bullet points in bolds
+
+               SHow everyting in proper format. you can use docx or format. THis format should be as follows
+               Output must follow word or docx format, strictly in the following manner
+               Title - Title of meeting
+               Heading : Meeting Agenda
+               Subheading - Name of attendies (if attendees name is not there , then keep it NA)
+               subheading - Date  of meeting and place of meeting(place means - name of conference/meeting room, if not provided then keep it online)
+               Body : The body must follow the following sequence of points:
+               * KEY POINTS DISCUSSED
+               * HIGHLIGHT ANY DECISION THAT HAS BEEN FINALIZED
+               * MENTION ACTIONABLE ITEMS 
+               * ANY ADDITIONAL NOTES
+               * ANY DEADLINE THAT HAS BEEN DISCUSSED
+               * ANY NEXT MEETING DATE THAT HAS BEEN DISCUSSED
+               * 2 to 3 line of summary
+               * Use Bullet points and highlight or bold important keywords such as CONTEXT IS CLEAR 
+               * Generate the output in such a way that it can be copied and pasted in word 
+
+               data provided by user is as follows : {user_text} '''
+  
+               response = model.generate_content(prompt)
+               st.write(response.text)
+               
+
+               st.download_button(label='Click to Download',
+                                   data = response.text,
+                                  file_name= 'MOM.txt',
+                                   mime='text/plain' )
